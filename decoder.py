64b8d87669a4a1652cc48a49a361b04eec09b892
swooboo <swooboo@mail.ru>
2016-05-01 18:16:33 +0300

    Decoder object now loads the configuration JSON upon creation.

--- decoder.py
+++ decoder.py
@@ -1,3 +1,5 @@
+import json
+
 '''
 Decoder wrapper. This class is responsible for conversing with the translation model.
 '''
@@ -6,6 +8,8 @@ class Decoder:
 	def __init__(self):
 		self.src = 'en'
 		self.dest = 'he'
+		with open('decoder.config.json') as config_file:
+			config = json.load(config_file)
 
 	'Gets tokens, returns translated tokens from the translation model'
 	def decode(self, tokens):
