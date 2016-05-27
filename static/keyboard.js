818ed78afe15aa40331615c142479e1ad746265a
swooboo <swooboo@mail.ru>
2016-05-28 02:19:46 +0300

    SITE: Not transliterating if CTRL is pressed. CTRL+T works now.

--- static/keyboard.js
+++ static/keyboard.js
@@ -12,7 +12,7 @@ defer(function(){ // Waiting for jQuery
 	
 
 	$("#source").keypress(function(evt) { // Overriding default key press to transliterate English key strokes to hebrew letters
-		if (evt.which) {
+		if (evt.which && !evt.ctrlKey) {	// Will try to translate if there is a key press and no CTRL pressed.
 			var charStr = String.fromCharCode(evt.which).toLowerCase();
 			var transformedChar = translateChar(charStr);
 			if (transformedChar != charStr) {	// If transliteration is indeed needed - paste the char and don't do the default keypress behavior
