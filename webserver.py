d8a2cbe516ac7536332b803b64d6c4211fd7579b
swooboo <swooboo@mail.ru>
2016-08-14 19:53:44 +0300

    Add exception when translate() fails

--- webserver.py
+++ webserver.py
@@ -3,6 +3,7 @@ import sys
 sys.dont_write_bytecode = True # Don't want cluttering .pyc files.
 from main import translate
 from flask import Flask, request, json
+from socket import error as socket_error
 app = Flask(__name__, static_url_path='')
 
 @app.route("/")
@@ -12,8 +13,12 @@ def index():
 @app.route("/tr", methods=['POST', 'GET'])
 def tr():
 	sentence = request.form.get("sentence")	# Got the sentence.
-	sentence = translate(sentence)	# Translating from the main.translate() function.
-	return json.jsonify(sentence=sentence)
+	try:
+		sentence = translate(sentence)	# Translating from the main.translate() function.
+		return json.jsonify(sentence=sentence)
+	except socket_error as error:
+		print "Exception socket_error: " + str(type(error))
+		return json.jsonify(error="couldn't translate")
 
 if __name__ == "__main__":
 	app.debug = True	# Debugging.
