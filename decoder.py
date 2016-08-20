7bf4293f97d77108948610b070edc828f5a3a713
swooboo <swooboo@mail.ru>
2016-08-20 20:09:34 +0300

    Chech Moses startup exit status

--- decoder.py
+++ decoder.py
@@ -16,7 +16,9 @@ class Decoder:
 	def init_server(self, config):
 		if 0 != os.system(config['check_cmd']):	# Checking if there is a moses process running
 			print("Moses daemon appears to be down. Attempting to start with command `" + config['start_cmd'] + "`.")
-			os.system(config['start_cmd']) # Making sure Moses server is up
+			exit_status = os.system(config['start_cmd'])>>8 # Making sure Moses server is up, shift right to get exit status
+			if 0 != exit_status:	# Checking exit status of startup command
+				print("Could not start Moses daemon. Starting command returned exit status " + str(exit_status) + ". Please check the Moses log file, or try to run manually without the --server switch.")
 		self.server = xmlrpclib.ServerProxy(config['host']) # Created a server object for the decoder
 
 	'Gets tokens, returns translated tokens from the translation model'
